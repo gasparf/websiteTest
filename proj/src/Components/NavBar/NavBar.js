38ac91b952d23a26e01ae71c16969621453cccdf
gasparf <gaspar.tan1189@gmail.com>
2021-12-29 02:45:56 -0800

    init

---
+++ proj/src/Components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+
+function NavBar() {
+    const [sideBar, setSideBar] = useState(false);
+
+    const showSidebar = () => setSideBar(!sideBar);
+
+    return(
+        <>
+        
+            <div className="navbar">
+                
+
+
+            </div>
+        
+        
+        
+        </>
+    )
+}
+
